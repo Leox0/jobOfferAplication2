745f24b9b2a1a779d5b9a41b8cb3d5afe164c26a
PCz <mail.00.kontakt@gmail.com>
2020-08-20 22:29:08 +0200

    first commit

---
+++ src/main/java/pl/sda/jobOfferAplication2/JobOfferAplication2Application.java
@@ -0,0 +1,13 @@
+package pl.sda.jobOfferAplication2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferAplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferAplication2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/jobOfferAplication2/JobOfferAplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.jobOfferAplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferAplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
