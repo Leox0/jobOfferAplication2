704d0e9cbbb3bfa4638918c577e98ba2f8a2959a
PCz <mail.00.kontakt@gmail.com>
2020-08-21 13:57:46 +0200

    UserException handler

--- src/main/java/pl/sda/jobOfferAplication2/user/controller/UserControllerAdvice.java
+++ src/main/java/pl/sda/jobOfferAplication2/user/controller/UserControllerAdvice.java
@@ -1,4 +1,17 @@
 package pl.sda.jobOfferAplication2.user.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.sda.jobOfferAplication2.user.exception.UserException;
+
+@ControllerAdvice
 public class UserControllerAdvice {
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<String> handUserException(UserException e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
 }
